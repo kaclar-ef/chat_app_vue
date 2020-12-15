f440aa507191fbd8e658f283a7b3bfaa43bad4ad
kaclar-ef <62864518+kaclar-ef@users.noreply.github.com>
2020-12-16 00:15:59 +0900

    public/javascripts/bundle.jsを読み込めるように設定

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module ChatAppVue
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.paths << Rails.root.join("public/javascripts")
   end
 end
